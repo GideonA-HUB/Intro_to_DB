796901ada1783ce7ca86d7fbcdd09e010d07e53c
Gideon Amienghemhen <amienz0958@gmail.com>
2025-02-03 02:48:44 +0100

    Add task_2.sql script to create tables in alx_book_store

--- task_2.sql
+++ task_2.sql
@@ -1,36 +1,39 @@
+USE alx_book_store;
+
 CREATE TABLE IF NOT EXISTS authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
-    author_name VARCHAR(215) NOT NULL
+    name VARCHAR(100) NOT NULL,
+    country VARCHAR(50)
 );
 
 CREATE TABLE IF NOT EXISTS books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(130) NOT NULL,
+    title VARCHAR(200) NOT NULL,
     author_id INT,
-    price DOUBLE NOT NULL,
-    publication_date DATE,
+    price DECIMAL(10, 2) NOT NULL,
+    publication_year INT,
     FOREIGN KEY (author_id) REFERENCES authors(author_id)
 );
 
 CREATE TABLE IF NOT EXISTS customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
-    address TEXT NOT NULL
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone VARCHAR(15)
 );
 
 CREATE TABLE IF NOT EXISTS orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE,
+    order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE IF NOT EXISTS order_details (
-    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
+    quantity INT NOT NULL,
     FOREIGN KEY (order_id) REFERENCES orders(order_id),
     FOREIGN KEY (book_id) REFERENCES books(book_id)
 );
